e49857536f057a1474ee83f51235158346b98ed2
Stephánie Costta <stephanie.costa.moura@hotmail.com>
2021-03-23 01:22:38 +0000

    Mapeado a tabela clientes

---
+++ src/main/java/br/gov/sp/fatec/springbootloja/entity/Clientes.java
@@ -0,0 +1,63 @@
+package br.gov.sp.fatec.springbootloja.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "clientes")
+public class Clientes {
+    
+    @Id
+    @Column(name = "id_cliente")
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+   
+  
+}
