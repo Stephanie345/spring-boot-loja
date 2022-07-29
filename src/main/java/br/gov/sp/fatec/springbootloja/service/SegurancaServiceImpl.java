b71e385cddbceebbcfef3d3fd2163edaf0f3b1b7
Stephánie Costta <stephanie.costa.moura@hotmail.com>
2022-07-29 16:06:28 -0300

    Criado a pasta security e realizado a autenticacao na busca de todas as vendas

--- src/main/java/br/gov/sp/fatec/springbootloja/service/SegurancaServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/springbootloja/service/SegurancaServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -51,6 +52,7 @@ public class SegurancaServiceImpl implements SegurancaService {
         return vendas;
     }
     @Override
+    @PreAuthorize("isAuthenticated()")
     public List<Vendas> buscarTodasVendas(){
         return vendasRepo.findAll();
         
